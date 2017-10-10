c309cd7d39abfa9eed248e88d65c8fb6c872e516
Justin Paul Ong <ong.jp08@gmail.com>
2017-10-10 14:27:55 +0800

    Error: video can't play

--- app/src/main/java/co/blackfintech/nasatv/activities/StreamActivity.kt
+++ app/src/main/java/co/blackfintech/nasatv/activities/StreamActivity.kt
@@ -1,5 +1,36 @@
 package co.blackfintech.nasatv.activities
 
+import android.os.Bundle
+import co.blackfintech.nasatv.R
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_stream.*
+
 /**
  * Created by User on 10/10/2017.
  */
+class StreamActivity : AppCompatActivity() {
+
+    companion object {
+        var STREAM_LINK: String = ""
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stream)
+
+        println(STREAM_LINK)
+        bindView()
+    }
+
+    private fun bindView() {
+        nasaStreamVideo.setZOrderOnTop(false);
+        nasaStreamVideo.setOnPreparedListener { mp ->
+            mp.start()
+        }
+        val videoUrl: Uri = Uri.parse(intent.getStringExtra(STREAM_LINK))
+        nasaStreamVideo.setVideoURI(videoUrl)
+    }
+}
